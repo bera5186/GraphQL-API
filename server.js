d69129a7ce33c24157dc178e5a14a2ad010d7074
bera5186 <rbasu611@gmail.com>
2019-09-09 21:06:57 +0530

    Updated the server to serve data over graphql API

--- server.js
+++ server.js
@@ -1,16 +1,48 @@
 var express = require('express')
 var egql = require('express-graphql')
 var { buildSchema } = require('graphql')
+const data = require('./db')
 
 // GraphQL Schema
 var schema = buildSchema(`
     type Query {
-        message: String
+        course(id: Int!): Course
+        courses(topic: String): [Course]
+    }
+
+    type Course {
+        id: Int
+        title: String
+        author: String
+        description: String
+        topic: String
+        url: String
     }
 `);
 
+var getCourse = (args) => {
+    var id = args.id;
+    return data.filter((course) => {
+        return course.id == id
+    })[0]
+}
+
+var getCourses = (args) => {
+    if(args.topic){
+        var topic = args.topic
+        return data.filter((course) => {
+            course.topic === topic
+        })
+
+    } else {
+        return data
+    }
+}
+
+// Root Resolver
 var root = {
-    message: () => 'Hello world'
+    course: getCourse,
+    courses: getCourses
 }
 
 var app = express()
@@ -19,5 +51,7 @@ app.use('/graphql', egql({
     rootValue: root,
     graphiql: true
 }))
-
-app.listen(4000, () => console.log('Server Running'))
+const PORT = 4000
+app.listen(PORT, () => {
+    console.log(`🚀 ⚡ Server Running at localhost:${PORT}/graphql ✨ ✨`)
+})
